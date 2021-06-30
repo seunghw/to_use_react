119f042cb8850d084351e8c2dc9203c4caa0a066
Seunghwan <ysp1307@naver.com>
2021-06-30 20:55:18 +0900

    fin

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# to_use_react
+# **to_use_react**
+
+<br>
+
+## **리액트 기초**
+
+---
+
+<br>
+
+리액트의 기초를 정리하고자 블로그 제작 및 기초 문법을 통해서 간단한 기능을 수행할 수 있는 웹 구현
+
+## **구현**
+
+---
+
+<img width="605" alt="스크린샷 2021-06-30 오후 8 13 46" src="https://user-images.githubusercontent.com/70190106/123951591-ea7e1d00-d9df-11eb-958f-2ba831d89c83.png">
+
+<br>
+<br>
+<br>
+
+## 기능
+
+---
+
+- CSS 디자인
+- navbar
+- 글 목록 UI
+- 버튼 이벤트
+- 모달창
+- 글 발행
+- 내용수정
+
+<br>
+
+<br>
+
+## 학습 내용
+
+---
+
+- 리액트 설치 및 개발환경 세팅
+  - VSC, Nodejs 설치
+  - npx create-react-app이란?
+
+* JSX 활용
+  - navbar 생성
+  - 데이터바인딩
+* state의 이해
+  - 중요데이터 저장
+  - state를 쓰는 이유
+  - 데이터 변경
+  - copy & spread 연산자
+* Component의 이해
+  - Component란?
+  - 주의점, 단점
+* 리액트에서의 if문
+  - 삼항연산자
+  - 클릭시 동작하는 UI
+* map/for
+  - map
+  - for
+* props에 대해서
+  - props 정리
+* input이란?
+  - onClick과 onChange
+  - key
--- blog/src/App.js
+++ blog/src/App.js
@@ -5,11 +5,15 @@ import './App.css';
 function App() {
 
   let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','연남 고기 맛집',]);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0,]);
 
   let[modal, modal변경] = useState(false);
 
   let posts = '광명 고기 맛집';
+
+  let [num, set_num] = useState(0);
+
+  let [input, setinput] = useState('바뀌어라!');
 /*
   function 제목바꾸기(){
     var newArray = [...글제목];
@@ -27,7 +31,7 @@ function App() {
         <div>개발 blog</div>
       </div>
 
-  
+{/*   
       <div className="list">
         <h3> { 글제목[0] } <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
@@ -42,14 +46,41 @@ function App() {
         <h3> { 글제목[2] } </h3>
         <p>2월 17일 발행</p>
         <hr/>
+      </div> */}
+
+      {
+        글제목.map((a,b)=>{
+          return (
+              <div className="list">
+                <h3 onClick= {() => {set_num(b)}}> { a } 
+                <span onClick={() => { 따봉변경( 따봉 + 1) }}>👍</span> {따봉} </h3>
+                <p>2월 17일 발행</p>
+                <hr/>
+              </div>
+          )
+        })
+      }
+
+{/* 
+      <input onChange={(e) => { setinput(e.target.value) } } /> */}
+
+      <div className="publish">
+        <input onChange={(e) => { setinput(e.target.value) }}/>
+        <button onClick={() => { 
+          var inputCopy = [...글제목];
+          inputCopy.unshift(input)
+          글제목변경( inputCopy ); 
+          }}>저장</button>
       </div>
+
+
 
       <button onClick={() => {  modal변경(!modal) }}>버튼</button>
       {
-      
-      modal === true
-      ? <Modal />
-      : null
+        
+        modal === true
+        ? <Modal title = {글제목} num = {num}/>
+        : null
 
       }
     
@@ -57,10 +88,10 @@ function App() {
   );
 }
 
-function Modal(){
+function Modal(props){
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{props.title[props.num]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
